dd982dd3f09fa8581fd78fc38d2521869dd36209
Jon Roby <jonathan.roby1@gmail.com>
2016-03-17 11:56:58 -0700

    First commit

---
+++ ReactFundamentals/Part1/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    )
+  }
+}
+
+//Stateful Component
+// class App extends React.Component {
+//   render() {
+//     return <div>Hi</div>
+//   }
+// }
+
+// Stateless Function Component
+// const App = () => <h1>Helloo Eggheads</h1>
+
+
+export default App;
